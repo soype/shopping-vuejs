11863c7233512d949da3ba2b8920d4a612a95510
soype <phenestroza@gmail.com>
2023-05-26 14:13:41 -0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+
+<template>
+  <div>
+    HI
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'App',
+  data: () => {
+    return {
+      products: [
+        {
+          name: "Hair Dryer",
+          price: 5.99,
+          img: 'https://www.sephora.com/productimages/sku/s1506526-main-zoom.jpg'
+        },
+        {
+          
+        }
+      ]
+    }
+  },
+  components: {}
+}
+</script>
+
+<style scoped>
+</style>
